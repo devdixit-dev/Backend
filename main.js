9f351d57cec1d9fa8e40286763db757c45ee89b0
Dev Dixit <devdixitsocial@gmail.com>
2025-04-16 12:22:59 +0530

    signup, login and logout code is ready, proceed for auth middleware

--- main.js
+++ main.js
@@ -11,10 +11,11 @@ app.use(express.json());
 app.use(cookieParser());
 
 const user_schema = new mongoose.Schema({
-  name: { type: String, required: true, minLength: 6 },
+  name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, minLength: 6 }
-});
+  password: { type: String, required: true },
+  activeSessionId: { type: String, default: null, unique: true }
+}, {timestamps: true});
 
 const User = mongoose.model('users', user_schema);
 
@@ -38,16 +39,16 @@ app.post('/user/signup', async (req, res) => {
   const user = await User.findOne({ email });
 
   if(user){
-    return res.json({
+    return res.status(404).json({
       success: false,
       message: "User already exist"
     });
   }
 
-  const decodePassword = bcrypt.hash(password, 10);
+  const decodePassword = await bcrypt.hash(password, 10);
 
   await User.create({ name, email, password: decodePassword })
-  return res.json({
+  return res.status(201).json({
     success: true,
     message: "User registered"
   });
@@ -76,14 +77,24 @@ app.post('/user/login', async (req, res) => {
     })
   }
 
-  const genID = await nanoid(8);
-  console.log(genID);
+  const genID = nanoid(12);
+  await User.updateOne(
+    { email: user.email }, 
+    { $set: { activeSessionId: genID } }
+  )
 
-  res.cookie('token', user._id, {
+  res.status(200)
+  .cookie('token', encodeURIComponent(user._id), {
     httpOnly: true, 
     secure: true,
     sameSite: true,
-    maxAge: 15 * 60 * 1000 // 15min
+    maxAge: 20 * 60 * 1000 // 20min
+  })
+  .cookie('activeSessionId', genID, {
+    httpOnly: true, 
+    secure: true,
+    sameSite: true,
+    maxAge: 20 * 60 * 1000 // 20min
   })
   .json({
     success: true,
@@ -91,14 +102,34 @@ app.post('/user/login', async (req, res) => {
   })
 });
 
-app.post('/user/logout', (req, res) => {
+app.post('/user/logout', async (req, res) => {
+
+  const token = req.cookies.token;
+
+  if(!token) {
+    console.log('Unauthorized login detected')
+    res.redirect('/user/login')
+  }
+
+  await User.updateOne(
+    { _id: token }, 
+    { $set: { activeSessionId: null } }
+  )
+
+  res.clearCookie('activeSessionId', {
+    httpOnly: true,
+    sameSite: 'strict',
+    secure: true
+  });
+
   res.clearCookie('token', {
     httpOnly: true,
     sameSite: 'strict',
     secure: true
   });
-  return res.json({
-    success: false,
+
+  return res.status(200).json({
+    success: true,
     message: "User logged out"
   })
 })
