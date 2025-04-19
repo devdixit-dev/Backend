bc5ab2b84e382d11d6e9bc9d3cc4182aac62503e
Dev Dixit <devdixitsocial@gmail.com>
2025-04-19 11:28:47 +0530

    integrate ejs for server side rendering

--- main.js
+++ main.js
@@ -4,13 +4,15 @@ import cookieParser from 'cookie-parser';
 import ConnectDatabase from './config/db.js'
 import userRouter from './routes/userRoute.js';
 
-
 const app = express();
 const port = 3000;
+
+app.set('view engine', 'ejs');
 
 ConnectDatabase();
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 app.use(cookieParser());
 
 app.use('/user', userRouter)
@@ -18,6 +20,14 @@ app.use('/user', userRouter)
 // home route
 app.get('/', (req, res) => {
   res.send('Home page or / page');
+});
+
+app.get('/signup', (req, res) => {
+  res.render('signup')
+});
+
+app.get('/login', (req, res) => {
+  res.render('login')
 });
 
 app.listen(port, () => { console.log(`Server is running on port ${port}`) });
